abf7f8b88279d7591bb13516caf411f00a09b707
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-15 01:21:38 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,7 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts["Freddy Mercury"].each do |key, value|
     if key == :favorite_ice_cream_flavors
-      key.delete("strawberry")
+      value.delete("strawberry")
     end
   end
   contacts
